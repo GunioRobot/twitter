91bdc7af06f9a63d669c6d468c1717e19ea5394b
Kegan Quimby <keganquimby@gmail.com>
2011-07-13 23:45:46 -0400

    signup errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,24 @@
 class UsersController < ApplicationController
   
-  def new
-    @title = "Sign Up"
-  end
-
   def show
     @user = User.find(params[:id])
     @title = @user.name
   end
 
+  def new
+    @title = "Sign Up"
+    @user = User.new    
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+
 end
+
